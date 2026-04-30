bdf7f8e498e4a731dfb058fc796956a769f24a6f
fedt-mand <142213779+fedt-mand@users.noreply.github.com>
2026-04-30 11:41:02 +0000

    Sort tags alphabetically in tag management dialog

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -86,7 +86,7 @@ export default function App() {
   };
 
   const getAllTags = () => {
-    return Array.from(tagHistory);
+    return Array.from(tagHistory).sort((a, b) => a.localeCompare(b, 'da'));
   };
 
   const clearWeek = () => {
